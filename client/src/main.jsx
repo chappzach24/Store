355625d4819542a83d1d53c7e4f4e57bd5a7af4d
zachary chapman <zach.chapman24@gmail.com>
2024-12-28 12:34:08 -0500

    updated navbar for auth admin, made protected route for admin page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,7 @@ import Home from "./pages/Home.jsx";
 import ErrorPage from "./components/ErrorPage.jsx";
 import Auth from "./pages/Auth.jsx";
 import AdminProducts from "./pages/AdminProducts.jsx";
+import ProtectedRoute from './components/ProtectedRoute';
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -30,8 +31,12 @@ const router = createBrowserRouter([
       },
       {
         path: "/admin/products",
-        element: <AdminProducts />,
-      },
+        element: (
+          <ProtectedRoute>
+            <AdminProducts />
+          </ProtectedRoute>
+        ),
+      }
     ],
   },
 ]);
